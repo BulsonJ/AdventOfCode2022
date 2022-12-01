7d37c1278695d283a6d82012231ffd652940d8b6
Jack Bulson <jacktbulson@gmail.com>
2022-12-01 23:39:54 +0000

    Cleanup day 1
    Cleanup day 1 with help from : https://rust-school.io/writing/aoc2022-day1/#setting-up-the-test

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-use std::env;
 use std::fs;
 
 fn main() {
-    day1();
-}
-
-fn day1() {
-    println!("Day one");
     let file_path = "input/day1.txt";
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
+    println!("PART 1: {}", day1_part_one(&contents));
+    println!("PART 2: {}", day1_part_two(&contents));
+}
+
+fn day1_part_one(input: &str) -> i32{
     let mut current_elf = 0;
     let mut elves = Vec::new();
-    for line in contents.split('\n') {
+    for line in input.split('\n') {
         if line == "\r" || line.is_empty() {
             elves.push(current_elf);
             current_elf = 0;
@@ -24,9 +23,23 @@ fn day1() {
         current_elf += number;
     }
     elves.sort();
-    let biggest: i32 = elves.iter().rev().take(1).sum();
-    println!("Elf:{biggest}");
+    elves.iter().rev().take(1).sum()
+}
 
-    let three_biggest: i32 = elves.iter().rev().take(3).sum();
-    println!("Three Biggest Elves:{three_biggest}");
+fn day1_part_two(input: &str) -> i32 {
+    let mut current_elf = 0;
+    let mut elves = Vec::new();
+    for line in input.split('\n') {
+        if line == "\r" || line.is_empty() {
+            elves.push(current_elf);
+            current_elf = 0;
+            continue;
+        }
+        let string = line.trim_end();
+        let number = string.parse::<i32>().unwrap();
+
+        current_elf += number;
+    }
+    elves.sort();
+    elves.iter().rev().take(3).sum()
 }
